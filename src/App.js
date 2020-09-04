332870ca98b1b4985ba6576af09d6037e1ff8eba
Bhabalomkar421 <Omkarbhabal11@gmail.com>
2020-09-04 20:57:30 +0530

    components build up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './components/navbar.component';
+import ExerciseList from './components/exercises-list.component';
+import EditExercise from './components/edit-exercise.component';
+import CreateExercise from './components/create-exercise.component';
+import CreateUser from './components/create-user.component';
+
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Route path="/" component={ExerciseList} exact={true}/>
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
-    </Router>
+    <div>
+      Navbar
+    </div>
   );
 }
 
 export default App;
+
+// <Router>
+//       <Navbar />
+//       <Route path="/" component={ExerciseList} exact={true}/>
+//       <Route path="/edit/:id" component={EditExercise} />
+//       <Route path="/create" component={CreateExercise} />
+//       <Route path="/user" component={CreateUser} />
+//     </Router>
